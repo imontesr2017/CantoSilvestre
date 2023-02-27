615692690570dfec80dee08275b54f5fc5a0ef18
imontesr2017 <85401502+imontesr2017@users.noreply.github.com>
2023-02-28 00:46:19 +0100

    Delete CantoSilvestre/src/main/java/Controladores directory
    Borrado de la antigua estructura

--- CantoSilvestre/src/main/java/Controladores/MensajeRepository.java
+++
@@ -1,8 +0,0 @@
-package Controladores;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
-public interface MensajeRepository extends JpaRepository<Mensaje, Long>{
-
-}
--- CantoSilvestre/src/main/java/Controladores/PajaroRepository.java
+++
@@ -1,7 +0,0 @@
-package Controladores;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PajaroRepository extends JpaRepository<Pajaro, Long>{
-
-}
